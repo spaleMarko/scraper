0b01cb44f8ba91f053f5b49ec73b6fe1bd04fb35
spaleMarko <marko.spasic@tangledgroup.com>
2024-02-08 19:27:07 +0100

    Add formater

--- index.mjs
+++ index.mjs
@@ -1,24 +1,24 @@
-import fs from 'fs'
+import fs from 'fs';
 import getData from './crawler/getData.mjs';
 
-const productsData = await getData()
+const productsData = await getData();
 
 // Check if productsData and if productsData.length not equal 0
-if(productsData && productsData.length > 0){
+if (productsData && productsData.length > 0) {
   const productListObject = productsData.reduce((acc, item) => {
     acc[item.position] = item;
     return acc;
   }, {});
-  
+
   // Convert Object to String
   const jsonContent = JSON.stringify(productListObject, null, 2);
 
   // Create JSON file
   fs.writeFile('productList.json', jsonContent, 'utf8', (err) => {
-      if (err) {
-          console.error('Something went wrong the list of  products:', err);
-          return;
-      }
-      console.log('The file was successfully saved as productList.json');
+    if (err) {
+      console.error('Something went wrong the list of  products:', err);
+      return;
+    }
+    console.log('The file was successfully saved as productList.json');
   });
 }
